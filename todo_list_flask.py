f0512f54957e24febfd3b373cdb3245feaebe185
tmontanaro <teodoro.montanaro@polito.it>
2016-04-11 13:01:12 +0200

    New version

--- todo_list_flask.py
+++ todo_list_flask.py
@@ -29,42 +29,31 @@ def hello_world():
 
 @app.route('/index.html')
 def index():
-    return render_template('index.html')
+    tasks_list = db_interaction.get_sorted_tasks_list()
+    return render_template('index.html', tasks_list=tasks_list)
 
 
-@app.route('/insert_task.html', methods=['GET', 'POST'])
+@app.route('/insert_task.html', methods=['POST'])
 def insert_task():
-    if request.method == 'POST':
-        if ('string_for_insertion' in request.form and request.form['string_for_insertion']!=''):
-            string_for_insertion = request.form['string_for_insertion']
-            if ('urgent_for_insertion' in request.form and request.form['urgent_for_insertion'] == 'on'):
-                urgent_for_insertion = 1
-            else:
-                urgent_for_insertion = 0
-            db_interaction.db_insert_task(string_for_insertion, urgent_for_insertion)
+    if ('string_for_insertion' in request.form and request.form['string_for_insertion']!=''):
+        string_for_insertion = request.form['string_for_insertion']
+        if ('urgent_for_insertion' in request.form and request.form['urgent_for_insertion'] == 'on'):
+            urgent_for_insertion = 1
         else:
-            string_for_insertion = ""
-    else:
-        string_for_insertion = ""
-    return render_template('insert_task.html', string_for_insertion = string_for_insertion)
+            urgent_for_insertion = 0
+        db_interaction.db_insert_task(string_for_insertion, urgent_for_insertion)
 
-@app.route('/show_tasks.html')
-def show_tasks():
-    tasks_list = db_interaction.get_sorted_tasks_list()
-    return render_template('show_tasks.html', tasks_list=tasks_list)
+    # back to the home page
+    return redirect(url_for('index'))
 
-@app.route('/delete_task.html', methods=['GET', 'POST'])
+@app.route('/delete_task.html', methods=['GET'])
 def delete_task():
-    if request.method == 'POST':
-        if 'substring_for_delete' in request.form:
-            substring_for_delete = request.form['substring_for_delete']
-            db_interaction.db_remove_task(substring_for_delete)
-        else:
-            substring_for_delete = ""
-    else:
-        substring_for_delete = ""
-    tasks_list = db_interaction.get_sorted_tasks_list()
-    return render_template('delete_task.html', substring_for_delete = substring_for_delete, tasks_list=tasks_list)
+    if 'string_for_delete' in request.args:
+        string_for_delete = request.args.get('string_for_delete')
+        db_interaction.db_remove_task(string_for_delete)
+
+    # back to the home page
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
